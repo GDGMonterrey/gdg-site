3a7f0dde603a9590d73ab7f4f6df3fd97a856ab6
dezkareid <elmaildeldezkareid@gmail.com>
2015-01-14 23:30:49 -0600

    Inclusión de Polymer y templating

--- hello.go
+++ hello.go
@@ -1,61 +1,37 @@
 package hello
 
 import (
-    "fmt"
     "html/template"
     "net/http"
-    "path/filepath"
+    //"path/filepath"
 )
 
 func init() {
-    http.HandleFunc("/", root)
-    http.HandleFunc("/sign", sign)
+    http.HandleFunc("/", home)
+    http.HandleFunc("/posts", posts)
 }
 
-func root(w http.ResponseWriter, r *http.Request) {
-    fmt.Fprint(w, guestbookForm)
+func home(w http.ResponseWriter, r *http.Request) {
+  d := map[string]interface{}{"Titulo": "GDG Monterrey", "Tab" : 0}
+  t := template.Must(template.ParseFiles("templates/root.html", "templates/base.html"))
+  err := t.ExecuteTemplate(w,"base", d)
+  if err != nil {
+    http.Error(w, err.Error(), http.StatusInternalServerError)
+  } 
 }
 
-const guestbookForm = `
-<html>
-  <body>
-    <form action="/sign" method="post">
-      <div><textarea name="content" rows="3" cols="60"></textarea></div>
-      <div><input type="submit" value="Sign Guestbook"></div>
-    </form>
-  </body>
-</html>
-`
-type Person struct {
-    Name string //exported field since it begins with a capital letter
+type Site struct {
+    Titulo string 
+    Tab int
 }
-func sign(w http.ResponseWriter, r *http.Request) {
-    //p := Person{Name:"Mary"}
-    d := map[string]interface{}{"Name": "Gold"}
-    if t, err := template.ParseFiles(filepath.Join("", "root.html")); err != nil {
-    // Something gnarly happened.
-      http.Error(w, err.Error(), http.StatusInternalServerError)
-    } else {
-    // return to client via t.Execute
-      err := t.Execute(w, d)
-      if err != nil {
-          http.Error(w, err.Error(), http.StatusInternalServerError)
-      }
-    }
-   /* err := signTemplate.Execute(w, d)
-    //err := signTemplate.Execute(w, r.FormValue("content"))
-    if err != nil {
-        http.Error(w, err.Error(), http.StatusInternalServerError)
-    }*/
+func posts(w http.ResponseWriter, r *http.Request) {
+  d := Site {Titulo:"GGD Monterrey", Tab: 1}
+  //  d := map[string]interface{}{"titulo": "GDG Monterrey", "tab" : 1}
+  t := template.Must(template.ParseFiles("templates/posts.html", "templates/base.html"))
+  err := t.ExecuteTemplate(w,"base", d)
+      
+  if err != nil {
+    http.Error(w, err.Error(), http.StatusInternalServerError)
+  }                
+   
 }
-
-var signTemplate = template.Must(template.New("sign").Parse(signTemplateHTML))
-
-const signTemplateHTML = `
-<html>
-  <body>
-    <p>You wrote:</p>
-    <pre>{{.Name}}</pre>
-  </body>
-</html>
-`
